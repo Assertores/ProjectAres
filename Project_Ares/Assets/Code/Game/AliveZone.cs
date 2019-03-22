061513f77cae7f753a10576c4e704ce7c0fc3b4f
[Jam]Andreas <andreas.edmeier@web.de>
2019-03-22 12:23:08 +0100

    [PRO] AliveZone

---
+++ Project_Ares/Assets/Code/Game/AliveZone.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProjectAres {
+    [RequireComponent(typeof(Collider2D))]
+    public class AliveZone : MonoBehaviour {
+
+        private void OnTriggerExit2D(Collider2D collision) {
+            IDamageableObject tmp = collision.gameObject.GetComponent<IDamageableObject>();
+            if (tmp != null) {
+                print(collision.gameObject.name);
+                tmp.TakeDamage(int.MaxValue, out int a, true);
+            } else {
+                Destroy(collision.gameObject);
+            }
+        }
+    }
+}
