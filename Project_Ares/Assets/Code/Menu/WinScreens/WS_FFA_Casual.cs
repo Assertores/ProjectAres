3e66c4cd1b5539486a9203d9b140a525ca489592
[Jam]Andreas <andreas.edmeier@web.de>
2019-03-26 17:48:33 +0100

    [PRO] first Tests with Winscreens

---
+++ Project_Ares/Assets/Code/Menu/WinScreens/WS_FFA_Casual.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProjectAres {
+    public class WS_FFA_Casual : WinScreen {
+
+        [Header("References")]
+        [SerializeField] GameObject _pillar;
+        [SerializeField] Vector3 _rightMostPlayer;
+        [SerializeField] Vector3 _leftMostPlayer;
+
+        void Start() {
+            for (int i = 0; i < Player._references.Count; i++) {
+                //Player._references[i].transform.position = Vector3.Lerp(_rightMostPlayer)
+            }
+        }
+
+        // Update is called once per frame
+        void Update() {
+
+        }
+    }
+}
