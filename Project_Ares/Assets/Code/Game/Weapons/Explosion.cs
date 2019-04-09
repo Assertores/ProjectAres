d544716929759bd297ad81e720728b6e83e8e789
[GA] Andreas <andreas.edmeier@web.de>
2019-04-09 17:52:11 +0200

    [PRO] tryed somewhat with explosions and rockets

--- Project_Ares/Assets/Code/Game/Weapons/Explosion.cs
+++ Project_Ares/Assets/Code/Game/Weapons/Explosion.cs
@@ -3,20 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace ProjectAres {
+    [RequireComponent(typeof(Collider2D))]
     public class Explosion : MonoBehaviour {
 
         #region Variables
 
         [Header("Balancing")]
         [SerializeField] int m_baseDamage = 1;
-        [SerializeField] float m_range = 5;
+        [SerializeField] float m_baseKnockback = 3;
         [SerializeField] AnimationCurve m_fallOff;
 
         #endregion
         #region MonoBehaviour
 
         void Start() {
-
+            //animation von explosion abspielen
         }
 
         void Update() {
@@ -29,8 +30,9 @@ namespace ProjectAres {
         private void OnCollisionEnter2D(Collision2D collision) {
             IDamageableObject tmp = collision.gameObject.GetComponent<IDamageableObject>();
             if (tmp != null) {
-                int damage = (int)Mathf.Round(m_fallOff.Evaluate(Vector2.Distance(collision.transform.position, transform.position)) * m_baseDamage);
-                tmp.TakeDamage(damage, null);//eventuell doch in rocket mit rein schreiben wegen reference zu source player
+                Vector2 dir = collision.transform.position - transform.position;
+                float fallOff = m_fallOff.Evaluate(dir.magnitude);
+                tmp.TakeDamage((int)Mathf.Round(fallOff * m_baseDamage), null, dir.normalized * fallOff * m_baseKnockback);//eventuell doch in rocket mit rein schreiben wegen reference zu source player
             }
         }
 
