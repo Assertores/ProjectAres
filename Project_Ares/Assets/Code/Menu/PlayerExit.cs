2fa29833879487b2788170bd3d8f819016d73558
GAMES-ACADEMY\Maximilian.Raabe <Maximilian.Raabe@games-academy.de>
2019-04-10 17:02:34 +0200

    [PRO] Exit almost reworked still missing a reference

--- Project_Ares/Assets/Code/Menu/PlayerExit.cs
+++ Project_Ares/Assets/Code/Menu/PlayerExit.cs
@@ -2,22 +2,60 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace ProjectAres {
+namespace ProjectAres
+{
     [RequireComponent(typeof(Collider2D))]
-    public class PlayerExit : MonoBehaviour {
+    public class PlayerExit : MonoBehaviour
+    {
+        #region Variables
+        [Header("References")]
+        [SerializeField] GameObject m_exitHatch;
+        [SerializeField] GameObject m_exit;
 
+        [Header("Variables")]
+        [SerializeField] float m_exitTime;
+        private float m_time;
+        private int m_collInd = 0;
+
+        #endregion
+
+        private void Start()
+        {
+            m_time = Time.timeSinceLevelLoad;
+        }
         #region Physics
 
-        private void OnTriggerEnter2D(Collider2D collision) {
-            Player tmp = collision.gameObject.GetComponent<Player>();
-            if (tmp) {
-                tmp.Disconect();
-                if(Player.s_references.Count <= 0) {
-                    MenuManager._singelton?.Exit();
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            m_collInd++;
+            while (m_collInd != 0)
+            {
+                if (m_exitTime + m_time <= Time.timeSinceLevelLoad)
+                {
+                    m_exitHatch.SetActive(false);
+                    Player tmp = collision.gameObject.GetComponent<Player>();
+                    if (tmp)
+                    {
+                        tmp.Disconect();
+                        if (Player.s_references.Count <= 0)
+                        {
+                            MenuManager._singelton?.Exit();
+
+                        }
+
+
+                    }
+
                 }
+
             }
         }
 
-        #endregion
     }
 }
+
+
+        #endregion
+    
+
