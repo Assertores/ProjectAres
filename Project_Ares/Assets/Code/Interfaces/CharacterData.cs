56f45baf0d94391c319d214b4c0ad5cdffe0c207
[GA] Andreas <andreas.edmeier@web.de>
2019-04-12 19:03:37 +0200

    [ART] added IceCube

---
+++ Project_Ares/Assets/Code/Interfaces/CharacterData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProjectAres {
+    [System.Serializable]
+    public class CharacterData {
+        public GameObject Model;
+        public GameObject SMG;
+        public GameObject Rocked;
+    }
+}
