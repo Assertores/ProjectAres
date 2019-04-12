aa7c539d7568eddad5032e4e9e47371a5dbdb89f
GAMES-ACADEMY\Maximilian.Raabe <Maximilian.Raabe@games-academy.de>
2019-04-12 14:18:09 +0200

    [PRO] Startbutton now has Health and regens Health after a bit of time

--- Project_Ares/Assets/Code/Menu/StartGame.cs
+++ Project_Ares/Assets/Code/Menu/StartGame.cs
@@ -1,16 +1,72 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace ProjectAres {
     public class StartGame : MonoBehaviour, IDamageableObject {
+
+        
 
         #region IDamageableObject
 
         public bool m_alive { get; set; }
 
+        [SerializeField]private int m_maxHealth = 100;
+
+        float m_currentHealth;
+
+        public Text m_healthText;
+        public Text m_startText;
+
+        private float m_time;
+        [SerializeField] private float m_regenTime;
+        [SerializeField] private float m_regeneration;
+
+        void Start()
+        {
+            m_currentHealth = m_maxHealth;
+            m_healthText.text = (Mathf.RoundToInt(m_currentHealth)).ToString();
+            m_startText.text = "";
+        }
+
+        private void Update()
+        {
+            if (m_time + m_regenTime <= Time.timeSinceLevelLoad) {
+
+                m_currentHealth += m_regeneration*Time.deltaTime;
+                m_healthText.text = (Mathf.RoundToInt(m_currentHealth)).ToString();
+
+                if (m_currentHealth > m_maxHealth)
+                {
+
+                    m_currentHealth = m_maxHealth;
+
+                }
+
+
+            }
+
+        }
+
+
         public void TakeDamage(int damage, Player source, Vector2 force) {
-            MenuManager._singelton?.StartGame();
+
+            if (m_currentHealth > 0)
+            {
+                m_time = Time.timeSinceLevelLoad;
+                m_currentHealth -= damage;
+                m_healthText.text = (Mathf.RoundToInt(m_currentHealth)).ToString();
+
+            }
+            if (m_currentHealth == 0)
+            {
+                m_healthText.text = "";
+                m_startText.text = "Game is Starting";
+                StartCoroutine(ChangeScene(3.0f));
+ 
+
+            }
         }
 
         public void Die(Player source) {
@@ -22,5 +78,16 @@ namespace ProjectAres {
         }
 
         #endregion
+
+        IEnumerator ChangeScene(float m_wait)
+        {
+            
+            yield return new WaitForSeconds(m_wait);
+            
+            MenuManager._singelton?.StartGame();
+            
+        }
+
     }
-}
+
+  }
