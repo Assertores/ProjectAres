4998c805ef338ca84cc904c6efc055c8fda878d0
[GA] Andreas <andreas.edmeier@web.de>
2019-04-17 15:56:22 +0200

    [PRO] Fixed AssetPost
    and added ignore for UI

---
+++ Project_Ares/Assets/Code/Editor/AssetPost.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class AssetPost : AssetPostprocessor
+{
+    void OnPreprocessTexture()
+    {
+        TextureImporter textureImporter = (TextureImporter)assetImporter;
+        if (assetPath.Contains("UI"))//distinguishes different import settings for different asset types e.g. UI, Background etc.
+            return;
+
+        textureImporter.spritePixelsPerUnit = 512;
+        textureImporter.filterMode = FilterMode.Point;
+        textureImporter.maxTextureSize = 512;
+    }
+}
