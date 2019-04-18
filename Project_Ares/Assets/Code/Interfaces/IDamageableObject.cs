1f7f8571871f21137b0a73a9519714ea8472d4ce
[GA] JD <andreas.edmeier@web.de>
2019-04-19 00:25:13 +0200

    [PRO] documentation for Interfaces

--- Project_Ares/Assets/Code/Interfaces/IDamageableObject.cs
+++ Project_Ares/Assets/Code/Interfaces/IDamageableObject.cs
@@ -5,10 +5,30 @@ using UnityEngine;
 namespace ProjectAres {
     public interface IDamageableObject {
 
+        /// <summary>
+        /// ist dieses ding noch am leben oder nicht
+        /// </summary>
         bool m_alive { get; set; }
 
+        /// <summary>
+        /// fügt dem DamageableObject schaden zu
+        /// wird meist von HarmingObjects aufgerufen
+        /// </summary>
+        /// <param name="damage">die menge an schaden, die gemacht wurde</param>
+        /// <param name="source">referenc auf den spieler, der für den schaden verantwortlich ist. falls es keinen spieler gibt sollte diese variable "null" sein</param>
+        /// <param name="force">mit welcher kraft das object in welche richtung geschleudert werden soll</param>
         void TakeDamage(int damage, Player source, Vector2 force);
+
+        /// <summary>
+        /// wird diese funktion aufgerufen, soll das object sofort und ohne ausnahme serben
+        /// </summary>
+        /// <param name="source">wer für diesen tot verantwortlich ist. "null" falls es keinen gibt</param>
         void Die(Player source);
+
+        /// <summary>
+        /// falls jemand mal wissen will wie fiel leben das ding noch hat
+        /// </summary>
+        /// <returns>die health, die dieses object noch haben</returns>
         int GetHealth();
     }
 }
