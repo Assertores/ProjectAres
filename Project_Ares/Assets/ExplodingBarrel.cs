99db9e4b218cf6049af5bc7e1afa47df77c361fc
GAMES-ACADEMY\Maximilian.Raabe <Maximilian.Raabe@games-academy.de>
2019-04-23 14:06:13 +0200

    [PRO] ExplodingBarrel without infinite Loops :P

---
+++ Project_Ares/Assets/ExplodingBarrel.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProjectAres
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class ExplodingBarrel : MonoBehaviour, IDamageableObject
+    {
+        #region Variables
+        [Header("References")]
+        [SerializeField] GameObject m_explosion;
+
+        [Header("Balancing")]
+        [SerializeField] int m_maxHealth;
+        int m_currentHealth;
+
+        bool m_isExploded = false;
+        Rigidbody2D m_rb;
+
+        #endregion
+
+        #region IDamageableObject
+
+
+        public bool m_alive { get; set; }
+
+        public void Die(Player source) {
+            throw new System.NotImplementedException();
+        }
+
+        public int GetHealth() {
+            throw new System.NotImplementedException();
+        }
+
+        public void TakeDamage(int damage, Player source, Vector2 force) {
+            m_currentHealth -= damage;
+            m_rb.AddForce(force);
+            if(!m_isExploded && m_currentHealth <= 0) {
+                m_isExploded = true;
+                Instantiate(m_explosion, transform.position, transform.rotation).GetComponentInChildren<IHarmingObject>()?.Init(source);
+                Destroy(gameObject);
+
+            }
+
+            
+        }
+        #endregion
+
+        #region MonoBehaviour
+        void Start()
+        {
+          
+            m_currentHealth = m_maxHealth;
+            m_rb = GetComponent<Rigidbody2D>();
+        }
+
+        #endregion
+    }
+
+}
