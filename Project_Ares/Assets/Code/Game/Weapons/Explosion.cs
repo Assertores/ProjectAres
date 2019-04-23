2f8fe08b42e008f0d8d5f028bc6a6a4153257493
GAMES-ACADEMY\Maximilian.Raabe <Maximilian.Raabe@games-academy.de>
2019-04-23 14:10:50 +0200

    [PRO] Deleted Debug.LogErrors in Explosionscript

--- Project_Ares/Assets/Code/Game/Weapons/Explosion.cs
+++ Project_Ares/Assets/Code/Game/Weapons/Explosion.cs
@@ -41,44 +41,21 @@ namespace ProjectAres {
         #region IHarmingObject
 
         public Rigidbody2D Init(Player reference) {
-            Debug.LogError("Crash1");
-            
             m_explosion.SetActive(true);
-            
-            Debug.LogError("Crash2");
-            
             m_time = Time.timeSinceLevelLoad;
-            Debug.LogError("Crash3");
             
             if (m_sounds.Length > 0) {
-                Debug.LogError("Crash4");
-                
                 m_audio.PlayOneShot(m_sounds[Random.Range(0, m_sounds.Length - 1)]);
-                Debug.LogError("Crash5");
-                
             }
             
             CameraShake.DoCamerashake(0.1f, 0.7f);
-            Debug.LogError("Crash6");
-            
+         
             foreach (var it in Physics2D.OverlapCircleAll(transform.position, m_radius)) {
-                Debug.LogError("Crash7");
-                
-                IDamageableObject tmp = it.gameObject.GetComponent<IDamageableObject>();
-                Debug.LogError("Crash8");
-                
-                if (tmp != null) {
-                    Debug.LogError("Crash9");
-                    
+                 IDamageableObject tmp = it.gameObject.GetComponent<IDamageableObject>();
+                 if (tmp != null) {
                     Vector2 dir = it.transform.position - transform.position;
-                    Debug.LogError("Crash10");
-                    
                     float fallOff = m_fallOff.Evaluate(dir.magnitude/m_radius);
-                    Debug.LogError("Crash11");
-                    
                     tmp.TakeDamage((int)Mathf.Round(fallOff * m_baseDamage), reference == null ? null : reference, dir.normalized * fallOff * m_baseKnockback);//eventuell doch in rocket mit rein schreiben wegen reference zu source player
-                    Debug.LogError("Crash12");
-                    return null;
                 }
             }
             return null;
