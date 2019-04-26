61c25232318989f36daabad5fa452ece8992046e
[GA] Andreas <andreas.edmeier@web.de>
2019-04-26 12:55:12 +0200

    [PRO] added CameraBoundingBox

---
+++ Project_Ares/Assets/Code/CameraStartAdaption.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProjectAres {
+    [RequireComponent(typeof(Camera))]
+    public class CameraStartAdaption : MonoBehaviour {
+
+        [SerializeField] BoxCollider2D m_boundingBox;
+
+        void Start() {
+            if (!m_boundingBox) {
+                Destroy(this);
+                return;
+            }
+            float tmpZ = transform.position.z;
+            transform.position = m_boundingBox.transform.position;
+            transform.position += new Vector3(m_boundingBox.offset.x, m_boundingBox.offset.y, 0);
+            transform.position = new Vector3(transform.position.x, transform.position.y, tmpZ);
+            Camera cam = GetComponent<Camera>();
+            cam.orthographicSize = m_boundingBox.size.y / 2;
+            if(cam.orthographicSize * cam.aspect < m_boundingBox.size.x / 2) {
+                cam.orthographicSize = (m_boundingBox.size.x / 2) / cam.aspect;
+            }
+
+            Destroy(m_boundingBox);
+            Destroy(this);
+            return;
+        }
+    }
+}
