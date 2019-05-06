11f5435a7517c4156f9819aa8c260e28cc8f9afa
[GA] Andreas <andreas.edmeier@web.de>
2019-05-06 12:11:49 +0200

    [PRO] created Kill Feed

---
+++ Project_Ares/Assets/Code/ReferenceHolder/KillFeedRefHolder.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+namespace ProjectAres {
+    public class KillFeedRefHolder : MonoBehaviour {
+        public TextMeshProUGUI m_killerName;
+        public Image m_killerIcon;
+        public Image m_weaponIcon;
+        public Image m_victimIcon;
+        public TextMeshProUGUI m_victimName;
+    }
+}
