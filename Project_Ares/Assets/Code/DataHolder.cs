344286c89b3f96e64a2259fdda4d4ec7abc3cf4a
[GA] Andreas <andreas.edmeier@web.de>
2019-05-06 17:31:26 +0200

    [PRO] added global DataHolder

---
+++ Project_Ares/Assets/Code/DataHolder.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProjectAres {
+    public static class DataHolder {
+
+        public static bool[] s_players = new bool[5];
+        public static string s_LevelName;
+        //public static string s_gameMode;
+    }
+}
