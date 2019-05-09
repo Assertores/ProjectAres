c74aa8e84d1af1c1a6b77693af6942c8d260799c
[GA] Andreas <andreas.edmeier@web.de>
2019-05-09 18:26:15 +0200

    [PRO] added piller Ref

---
+++ Project_Ares/Assets/Code/ReferenceHolder/PillarRefHolder.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+namespace ProjectAres {
+    public class PillarRefHolder : MonoBehaviour {
+        public TextMeshProUGUI m_screen;
+        public TextMeshProUGUI m_playerName;
+        public TextMeshProUGUI m_pillarField;
+        public Image m_pillarGradient;
+    }
+}
