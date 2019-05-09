89b2d90d40619d1b4d6163e80fc534dcd8aa3d54
[GA] Andreas <andreas.edmeier@web.de>
2019-05-09 18:26:56 +0200

    [PRO] seperated spawn behaviour

--- Project_Ares/Assets/Code/Menu/MenuManager.cs
+++ Project_Ares/Assets/Code/Menu/MenuManager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using XInputDotNetPure;
+
 
 namespace ProjectAres {
     public class MenuManager : MonoBehaviour {
@@ -10,10 +10,7 @@ namespace ProjectAres {
         #region Variables
 
         [Header("References")]
-        [SerializeField] GameObject m_playerRev;
         [SerializeField] GameObject m_SpawnPoint;
-
-        GamePadState[] m_lastStates = new GamePadState[4];
 
         #endregion
         #region MonoBehaviour
@@ -43,67 +40,7 @@ namespace ProjectAres {
             }
         }
 
-        private void Update() {
-            if (!DataHolder.s_players[4] && Input.GetKeyDown(KeyCode.Return)) {
-                GameObject tmp = Instantiate(m_playerRev);
-                if (tmp) {
-                    DataHolder.s_players[4] = true;
-                    GameObject tmpControle = new GameObject("Controler");
-                    //tmpControle.transform.parent = tmp.transform;
-
-                    IControl reference = tmpControle.AddComponent<KeyboardControl>();//null reference checks
-
-                    //tmp.GetComponent<Player>().Init(KeyboardControl);
-
-                    tmp.transform.position = m_SpawnPoint.transform.position;
-
-                    tmp.GetComponentInChildren<Player>().Init(tmpControle);//dirty
-                    //tmp.GetComponentInChildren<Player>().Init(reference);//null reference checks
-                    tmp.GetComponentInChildren<Player>().Invincible(true);//TODO: playerscript wird doppeld gesucht
-                    tmp.GetComponentInChildren<Player>().SetChangeCharAble(true);
-                }
-            }
-            for(int i = 0; i < 4; i++) {
-                if(!DataHolder.s_players[i] && m_lastStates[i].IsConnected && m_lastStates[i].Buttons.Start == ButtonState.Pressed && GamePad.GetState((PlayerIndex)i).Buttons.Start == ButtonState.Released) {
-                    GameObject tmp = Instantiate(m_playerRev);
-                    if (tmp) {
-                        DataHolder.s_players[i] = true;
-                        GameObject tmpControle = new GameObject("Controler");
-                        //tmpControle.transform.parent = tmp.transform;
-
-                        ControllerControl reference = tmpControle.AddComponent<ControllerControl>();//null reference checks
-                        reference.m_controlerIndex = i;
-
-                        tmp.transform.position = m_SpawnPoint.transform.position;
-
-                        tmp.GetComponentInChildren<Player>().Init(tmpControle);//dirty
-                        //tmp.GetComponent<Player>().Init(reference);//null reference checks
-                        tmp.GetComponentInChildren<Player>().Invincible(true);//TODO: playerscript wird doppeld gesucht
-                        tmp.GetComponentInChildren<Player>().SetChangeCharAble(true);
-
-                    }
-                }
-                m_lastStates[i] = GamePad.GetState((PlayerIndex)i);
-            }
-
-            if (Input.GetKeyDown(KeyCode.O))
-            {
-                GameObject tmp = Instantiate(m_playerRev);
-                if (tmp) {
-                    GameObject tmpController = new GameObject("KI Controller");
-                    //tmpControle.transform.parent = tmp.transform;
-
-                    IControl reference = tmpController.AddComponent<KI_Minion>();
-
-                    tmp.transform.position = m_SpawnPoint.transform.position;
-
-                    Player minion = tmp.GetComponentInChildren<Player>();
-                    minion.Init(tmpController);
-                    minion.Invincible(true);
-                    minion.SetChangeCharAble(true);
-                }
-            }
-        }
+        
 
         #endregion
 
@@ -113,7 +50,7 @@ namespace ProjectAres {
                 it.SetChangeCharAble(false);
             }
 
-            SceneManager.LoadScene(StringCollection.COLOSSEUM);
+            SceneManager.LoadScene(DataHolder.s_level);
             //SceneManager.LoadScene(StringCollection.EXAMPLESZENE);
             //lade ausgewählte Szene im hintergrund
             //spiel animation für szenenwechsel ab
