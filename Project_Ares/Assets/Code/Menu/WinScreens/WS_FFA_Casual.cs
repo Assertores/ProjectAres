8d3210d091c3d5a9e9dd719cffcee48df3adbc03
GAMES-ACADEMY\Maximilian.Raabe <Maximilian.Raabe@games-academy.de>
2019-05-10 00:18:46 +0200

    [PRO] Refactoring pillars in winscreen

--- Project_Ares/Assets/Code/Menu/WinScreens/WS_FFA_Casual.cs
+++ Project_Ares/Assets/Code/Menu/WinScreens/WS_FFA_Casual.cs
@@ -26,7 +26,7 @@ namespace ProjectAres {
 
         float m_pillarSpeed = 1;
         float m_hightPerKill = 1;
-        List<d_pillar> m_pillar = new List<d_pillar>();
+        List<PillarRefHolder> m_pillar = new List<PillarRefHolder>();
         float m_startTime;
 
         #endregion
@@ -34,11 +34,15 @@ namespace ProjectAres {
         #region MonoBehaviour
 
         void Start() {
-            if(DataHolder.s_gameMode != e_gameMode.FFA_CASUAL) {
+            if (DataHolder.s_gameMode != e_gameMode.FFA_CASUAL) {
                 Destroy(this);
                 return;
             }
-
+            if (!m_pillarRef.GetComponent<PillarRefHolder>()) {
+                print("no pillar ref on the Prefab");
+                return;
+            }
+            
             int maxKills = 0;
             Directory.CreateDirectory(StringCollection.DATAPATH);
             for (int i = 0; i < Player.s_references.Count; i++) {
@@ -57,11 +61,13 @@ namespace ProjectAres {
 
                 Player.s_references[i].transform.position = Vector3.Lerp(m_leftMostPlayer.position, m_rightMostPlayer.position, ((float)i+1) /(Player.s_references.Count+1));
                 Player.s_references[i].InControle(false);
-                d_pillar tmp;
-                tmp.pillar = Instantiate(m_pillarRef, Player.s_references[i].transform.position, Player.s_references[i].transform.rotation);
-                tmp.text = tmp.pillar.GetComponentInChildren<TextMeshProUGUI>();
-                m_pillar.Add(tmp);
-                m_pillar[i].text.text = "0";
+                m_pillar.Add(Instantiate(m_pillarRef, Player.s_references[i].transform.position, Player.s_references[i].transform.rotation).GetComponent<PillarRefHolder>());
+
+
+                //----- ----- FeedBack ----- -----
+                
+                m_pillar[i].m_screen.text = "0";
+                m_pillar[i].m_playerName.text = Player.s_references[i].m_stats.m_name;
             }
 
             m_pillarSpeed = (m_maxHeight.position.y - m_rightMostPlayer.position.y) / m_winScreenMaxTime;
@@ -72,11 +78,11 @@ namespace ProjectAres {
         // Update is called once per frame
         void Update() {
             for(int i = 0; i < Player.s_references.Count; i++) {
-                if(m_pillar[i].pillar.transform.position.y < m_leftMostPlayer.position.y + (Player.s_references[i].m_stats.m_kills * m_hightPerKill)) {
-                    m_pillar[i].text.text = Mathf.RoundToInt((m_pillar[i].pillar.transform.position.y - m_leftMostPlayer.position.y) / m_hightPerKill).ToString();
-                    m_pillar[i].pillar.transform.position += new Vector3(0,m_pillarSpeed * Time.deltaTime,0);
+                if(m_pillar[i].gameObject.transform.position.y < m_leftMostPlayer.position.y + (Player.s_references[i].m_stats.m_kills * m_hightPerKill)) {
+                    m_pillar[i].m_screen.text = Mathf.RoundToInt((m_pillar[i].gameObject.transform.position.y - m_leftMostPlayer.position.y) / m_hightPerKill).ToString();
+                    m_pillar[i].gameObject.transform.position += new Vector3(0,m_pillarSpeed * Time.deltaTime,0);
 
-                    Player.s_references[i].transform.position = m_pillar[i].pillar.transform.position;
+                    Player.s_references[i].transform.position = m_pillar[i].gameObject.transform.position;
                 } else {
                     Player.s_references[i].InControle(true);
                 }
