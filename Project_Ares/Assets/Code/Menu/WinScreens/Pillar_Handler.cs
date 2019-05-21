ef4048dae3892126a201d89e3f0872b3cd7080d5
[GA] Andreas <andreas.edmeier@web.de>
2019-05-21 15:28:23 +0200

    [PRO] started Pillar Handler

---
+++ Project_Ares/Assets/Code/Menu/WinScreens/Pillar_Handler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PPBC {
+    public class Pillar_Handler : MonoBehaviour {
+
+        #region Variables
+
+        [Header("References")]
+        [SerializeField] GameObject m_pillarPrefab;
+
+        #endregion
+        #region MonoBehaviour
+        void Start() {
+
+        }
+
+        // Update is called once per frame
+        void Update() {
+
+        }
+
+        #endregion
+    }
+}
