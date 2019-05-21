b3aa6770f445bb891dc0cc46e37539e222872aa7
[GA] Andreas <andreas.edmeier@web.de>
2019-05-21 16:59:30 +0200

    [PRO] EndScreenManager

---
+++ Project_Ares/Assets/Code/Menu/WinScreens/EndScreenManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PPBC {
+    public class EndScreenManager : MonoBehaviour {
+
+        #region Variables
+
+        [Header("References")]
+        [SerializeField] PillarHandler pillarHandler;
+
+        #endregion
+        #region MonoBehaviour
+
+        void Start() {
+            switch (DataHolder.s_gameMode) {
+            case e_gameMode.FFA_CASUAL:
+                break;
+            case e_gameMode.FAIR_TOURNAMENT:
+                break;
+            default:
+                break;
+            }
+
+            if (pillarHandler) {
+                pillarHandler.CallBack += NextOfPillarHandler;
+            }
+        }
+        
+        void Update() {
+
+        }
+
+        #endregion
+
+        void NextOfPillarHandler() {
+            switch (DataHolder.s_gameMode) {
+            case e_gameMode.FFA_CASUAL:
+                break;
+            case e_gameMode.FAIR_TOURNAMENT:
+                break;
+            default:
+                break;
+            }
+        }
+    }
+}
