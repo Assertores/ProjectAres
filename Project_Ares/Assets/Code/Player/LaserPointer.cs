e6a17641e9aa4d4338f9ca591fe0362c294bed83
[GA] Andreas <andreas.edmeier@web.de>
2019-05-23 16:52:40 +0200

    [PRO] added Lazer pointer

---
+++ Project_Ares/Assets/Code/Player/LaserPointer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PPBC {
+    [RequireComponent(typeof(LineRenderer))]
+    public class LaserPointer : MonoBehaviour {
+
+        LineRenderer m_laser;
+
+        void Start() {
+            m_laser = GetComponent<LineRenderer>();
+            //m_laser.enabled = false;
+        }
+
+        private void Update() {
+            Debug.DrawRay(transform.position, transform.right);
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.right);
+            if (hit.collider == null) {
+                print("hit nothing");
+                return;
+            }
+            print(hit.collider.name);
+            m_laser.SetPosition(1, new Vector2(Vector2.Distance(hit.point, transform.position),0));
+        }
+    }
+}
