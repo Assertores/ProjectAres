bab6c1d63969bab9a4122b6dca498d027234a2d8
[GA] Andreas <andreas.edmeier@web.de>
2019-05-24 17:20:26 +0200

    [PRO] created Map data

---
+++ Project_Ares/Assets/Code/MapEditor/MapDATA.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PPBC {
+    [CreateAssetMenu(menuName = "Map")]
+    public class MapDATA : ScriptableObject {
+        [System.Serializable]
+        public struct d_mapData {
+            public int index;
+            public Vector2 position;
+            public float rotation;
+            public Vector2 scale;
+        }
+        [System.Serializable]
+        public struct d_mapLights {
+            public Vector2 position;
+            public Color color;
+        }
+        [System.Serializable]
+        public struct d_mapPlayerStart {
+            public Vector2 position;
+            public int team;
+        }
+
+        public GameObject[] p_background;
+        public GameObject[] p_props;
+        public GameObject[] p_stage;
+        public GameObject[] p_forground;
+        public GameObject p_light;
+        public GameObject p_laserBariar;
+
+        public Vector2 m_size;
+        public int m_background;
+        public d_mapData[] m_props;
+        public d_mapData[] m_stage;
+        public d_mapData[] m_forground;
+        public d_mapLights[] m_lights;
+        public d_mapPlayerStart[] m_playerStarts;
+        public d_mapData[] m_border;
+        public AudioClip m_backgroundMusic;
+    }
+}
