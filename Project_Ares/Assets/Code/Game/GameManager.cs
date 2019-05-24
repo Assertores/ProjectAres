c6d202a7d82554c6e325f52f9e9b44a0361d4be2
[GA] Andreas <andreas.edmeier@web.de>
2019-05-24 17:27:24 +0200

    [PRO] moved levelstuff to common level transform

--- Project_Ares/Assets/Code/Game/GameManager.cs
+++ Project_Ares/Assets/Code/Game/GameManager.cs
@@ -11,6 +11,7 @@ namespace PPBC {
         [Header("References")]
         [SerializeField] BoxCollider2D m_cameraSize;
         [SerializeField] AudioSource m_backgroundAudio;
+        [SerializeField] Transform m_levelHolder;
 
         [HideInInspector]
         public List<Transform> m_borders { get; private set; } = new List<Transform>();
@@ -75,36 +76,44 @@ namespace PPBC {
 
         void LoadMap() {
             MapDATA currentMap = DataHolder.s_maps[DataHolder.s_map];
-            Instantiate(currentMap.p_background[currentMap.m_background],Vector3.zero, Quaternion.Euler(Vector3.zero));
+            GameObject a = Instantiate(currentMap.p_background[currentMap.m_background],Vector3.zero, Quaternion.Euler(Vector3.zero));
+            a.transform.parent = m_levelHolder;
             foreach(var it in currentMap.m_props) {
                 GameObject tmp = Instantiate(currentMap.p_props[it.index], it.position, Quaternion.Euler(new Vector3(0, 0, it.rotation)));
                 tmp.transform.localScale = it.scale;
+                tmp.transform.parent = m_levelHolder;
             }
             foreach (var it in currentMap.m_stage) {
                 GameObject tmp = Instantiate(currentMap.p_stage[it.index], it.position, Quaternion.Euler(new Vector3(0, 0, it.rotation)));
                 tmp.transform.localScale = it.scale;
+                tmp.transform.parent = m_levelHolder;
             }
             foreach (var it in currentMap.m_forground) {
                 GameObject tmp = Instantiate(currentMap.p_forground[it.index], it.position, Quaternion.Euler(new Vector3(0, 0, it.rotation)));
                 tmp.transform.localScale = it.scale;
+                tmp.transform.parent = m_levelHolder;
             }
             if (currentMap.p_light.GetComponent<Light>()) {
                 foreach (var it in currentMap.m_lights) {
                     GameObject tmp = Instantiate(currentMap.p_light, it.position, Quaternion.Euler(Vector3.zero));
                     tmp.GetComponent<Light>().color = it.color;
+                    tmp.transform.parent = m_levelHolder;
                 }
             }
             for (int i = 0; i < currentMap.m_playerStarts.Length; i++) {
                 GameObject tmp = new GameObject("PlayerSpawn " + i);
                 tmp.transform.position = currentMap.m_playerStarts[i].position;
                 tmp.AddComponent<PlayerStart>().team = currentMap.m_playerStarts[i].team;
+                tmp.transform.parent = m_levelHolder;
             }
             for (int i = 0; i < currentMap.m_border.Length; i++) {
                 GameObject tmp = Instantiate(currentMap.p_props[currentMap.m_border[i].index], currentMap.m_border[i].position, Quaternion.Euler(new Vector3(0, 0, currentMap.m_border[i].rotation)));
                 tmp.transform.localScale = currentMap.m_border[i].scale;
+                tmp.transform.parent = m_levelHolder;
                 m_borders.Add(tmp.transform);
             }
-            Instantiate(currentMap.p_laserBariar);
+            a = Instantiate(currentMap.p_laserBariar);
+            a.transform.parent = m_levelHolder;
 
             if (m_backgroundAudio) {
                 m_backgroundAudio.clip = currentMap.m_backgroundMusic;
