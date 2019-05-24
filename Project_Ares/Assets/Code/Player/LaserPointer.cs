a58fe5b7b242cbaeceb645203c6e1e30fd35ca75
[GA] Andreas <andreas.edmeier@web.de>
2019-05-24 12:53:30 +0200

    [PRO] optimice raycast for LaserPointer

--- Project_Ares/Assets/Code/Player/LaserPointer.cs
+++ Project_Ares/Assets/Code/Player/LaserPointer.cs
@@ -15,14 +15,14 @@ namespace PPBC {
             //m_laser.enabled = false;
         }
 
-        private void Update() {
+        private void FixedUpdate() {
             Debug.DrawRay(transform.position, transform.right);
             RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.right,1000f,m_layerMask);
             if (hit.collider == null) {
                 m_laser.SetPosition(1, new Vector2(100, 0));
                 return;
             }
-            print(hit.collider.name);
+
             m_laser.SetPosition(1, new Vector2(Vector2.Distance(hit.point, transform.position),0));
         }
     }
