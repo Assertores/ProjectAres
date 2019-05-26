65c724172949a138322369d0afe081b22e055544
[Jam]Andreas <andreas.edmeier@web.de>
2019-05-26 14:20:32 +0200

    [PRO] added Scaling and Rotate editing

--- Project_Ares/Assets/Code/MapEditor/EditorHUDAndPlayerLogic.cs
+++ Project_Ares/Assets/Code/MapEditor/EditorHUDAndPlayerLogic.cs
@@ -6,13 +6,19 @@ using TMPro;
 namespace PPBC {
     public class EditorHUDAndPlayerLogic : MonoBehaviour {
 
+        enum e_editingStyle { ROTATE = 0, MOVE = 1, SCALE = 2}
+
         [Header("References")]
         [SerializeField] TextMeshProUGUI m_typeRef;
         [SerializeField] TextMeshProUGUI m_IndexRef;
 
+        [Header("Balancing")]
+        [SerializeField] float m_scalerDampaning = 110;
+
         IControl m_controlRef;
         bool m_isDraging = false;
         ObjectReferenceHolder m_editObj;
+        e_editingStyle m_styple = e_editingStyle.MOVE;
 
         e_objType m_type;
         int m_rawIndex;
@@ -29,9 +35,27 @@ namespace PPBC {
 
         // Update is called once per frame
         void Update() {
-            transform.parent.position += new Vector3(m_controlRef.m_dir.x * Mathf.Sqrt(1 - ((m_controlRef.m_dir.y * m_controlRef.m_dir.y) / 2)), m_controlRef.m_dir.y * Mathf.Sqrt(1 - ((m_controlRef.m_dir.x * m_controlRef.m_dir.x) / 2)), 0);
+            if(m_styple == e_editingStyle.MOVE)
+                transform.parent.position += new Vector3(m_controlRef.m_dir.x * Mathf.Sqrt(1 - ((m_controlRef.m_dir.y * m_controlRef.m_dir.y) / 2)), m_controlRef.m_dir.y * Mathf.Sqrt(1 - ((m_controlRef.m_dir.x * m_controlRef.m_dir.x) / 2)), 0);
+
             if (m_isDraging) {
-                m_editObj.transform.position = transform.position;
+                
+                switch (m_styple) {
+                case e_editingStyle.ROTATE:
+                    m_editObj.m_objectHolder.rotation = Quaternion.LookRotation(transform.forward, new Vector2(-m_controlRef.m_dir.y, m_controlRef.m_dir.x));//vektor irgendwie drehen, damit es in der 2d plain bleibt
+                    break;
+                case e_editingStyle.MOVE:
+                    m_editObj.transform.position = transform.position;
+                    break;
+                case e_editingStyle.SCALE:
+                    float scaler = Vector2.SignedAngle(transform.up, m_controlRef.m_dir);
+                    scaler /= m_scalerDampaning;
+                    scaler = scaler * scaler * scaler;
+                    m_editObj.m_objectHolder.localScale = new Vector3(scaler, scaler, 1);
+                    break;
+                default:
+                    break;
+                }
             }
         }
 
@@ -41,9 +65,11 @@ namespace PPBC {
 
         public void DragHandler() {
             if (m_isDraging) {
-                m_isDraging = false;
+                Drop();
                 return;
             }
+
+            m_typeRef.text = m_styple.ToString();
 
             Collider2D[] hits = Physics2D.OverlapPointAll(transform.position);
             foreach (var it in hits) {
@@ -60,6 +86,19 @@ namespace PPBC {
             GameManager.s_singelton.m_mapHandler.LoadNewObj(CreateData());
         }
 
+        void Drop() {
+            if (m_isDraging) {
+                m_editObj.m_data.position = m_editObj.m_objectHolder.position;
+                m_editObj.m_data.rotation = m_editObj.m_objectHolder.rotation.eulerAngles.z;
+                m_editObj.m_data.scale = m_editObj.m_objectHolder.localScale;
+
+                m_isDraging = false;
+            }
+            m_styple = e_editingStyle.MOVE;
+
+            m_typeRef.text = m_type.ToString();
+        }
+
         d_mapData CreateData() {
             d_mapData tmp;
             tmp.type = m_type;
@@ -71,7 +110,16 @@ namespace PPBC {
         }
 
         public void ChangeType(bool forward) {
-            m_isDraging = false;
+            if (m_isDraging) {
+                if (forward) {
+                    m_styple++;
+                } else {
+                    m_styple--;
+                }
+                m_styple = (e_editingStyle)fixedMod((int)m_styple, 3);//reference to enum at top of this script
+                m_typeRef.text = m_styple.ToString();
+                return;
+            }
 
             if (forward)
                 m_type++;
@@ -112,11 +160,12 @@ namespace PPBC {
             }else if(m_type == e_objType.SIZE) {
                 GameManager.s_singelton.m_mapHandler.SetSize(m_index);
             }
-            else if (m_isDraging) {
+            else if (m_isDraging && m_editObj.m_objectHolder) {
                 foreach(Transform it in m_editObj.m_objectHolder) {
                     Destroy(it.gameObject);
                 }
-                GameManager.s_singelton.m_mapHandler.LoadObj(CreateData()).transform.parent = m_editObj.m_objectHolder;
+                m_editObj.m_data = CreateData();
+                GameManager.s_singelton.m_mapHandler.LoadObj(m_editObj.m_data).transform.parent = m_editObj.m_objectHolder;
             }
         }
 
@@ -136,7 +185,7 @@ namespace PPBC {
                 max = -1;
                 break;
             case e_objType.LIGHT:
-                max = 1;
+                max = DataHolder.s_maps[DataHolder.s_map].p_colors.Length;
                 break;
             case e_objType.FORGROUND:
                 max = DataHolder.s_maps[DataHolder.s_map].p_forground.Length;
