d76070c7add48362be269d059b9f3c739032255f
[GA] Max <maximilian_raabe@icloud.com>
2019-06-06 17:20:17 +0200

    [PRO] Added Crown behaviour and prefab

---
+++ Project_Ares/Assets/Code/Game/Crown.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PPBC {
+    public class Crown : MonoBehaviour
+    {
+        #region Variables
+        [Header("Balancing")]
+        [SerializeField] float m_lerpTime;
+        [SerializeField] float m_crownOffset;
+        #endregion
+        void LateUpdate() {
+            
+            transform.position = Vector2.Lerp(transform.position,new Vector2(Player.s_sortedRef[0].transform.position.x, Player.s_sortedRef[0].transform.position.y + Player.s_sortedRef[0].m_distanceToGround + m_crownOffset ),m_lerpTime);
+            
+        }
+    }
+}
