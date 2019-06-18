e90985759bb4b09731b468954088fbe8d74bb34e
[GA] Andreas <andreas.edmeier@web.de>
2019-06-18 16:08:49 +0200

    [PRO] follow object script

---
+++ Project_Ares/Assets/Code/Tools/FollowObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PPBC {
+    public class FollowObject : MonoBehaviour {
+
+        [SerializeField] Transform m_target;
+
+        void Start() {
+            if (!m_target) {
+                Destroy(this);
+                return;
+            }
+        }
+
+        void Update() {
+            transform.position = m_target.position;
+        }
+    }
+}
