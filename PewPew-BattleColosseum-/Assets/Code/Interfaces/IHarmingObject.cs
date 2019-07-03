489b73bfb2d634f7e90e518f75a2ce65a3b6842d
[GA] Andreas <andreas.edmeier@web.de>
2019-07-03 12:34:59 +0200

    [PRO] damage system interfaces

---
+++ PewPew-BattleColosseum-/Assets/Code/Interfaces/IHarmingObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace PPBC {
+    public enum e_HarmingObjectType { SMG, ROCKED, LASOR } //TODO: find name for wave at the end of the game
+    public interface IHarmingObject {
+
+        /// <summary>
+        /// icon of the object for the kill feed
+        /// </summary>
+        Sprite m_icon { get; }
+
+
+        /// <summary>
+        /// type of the object to destinguage it
+        /// </summary>
+        e_HarmingObjectType m_type { get; }
+
+        /// <summary>
+        /// the responcalbe player if he exists
+        /// </summary>
+        Player m_owner { get; }
+    }
+}
---
+++ PewPew-BattleColosseum-/Assets/Code/Interfaces/ITracer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PPBC {
+    public interface ITracer {
+
+        /// <summary>
+        /// reference to the source object
+        /// </summary>
+        IHarmingObject m_trace { get; }
+
+
+        /// <summary>
+        /// will be called once after instanciating
+        /// </summary>
+        /// <param name="trace">the source object</param>
+        void Init(IHarmingObject trace);
+    }
+}
