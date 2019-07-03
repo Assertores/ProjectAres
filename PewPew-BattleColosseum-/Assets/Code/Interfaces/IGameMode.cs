2f62948129bfe2ea3c92052a60fbc525732bd546
[GA] Andreas <andreas.edmeier@web.de>
2019-07-03 17:08:17 +0200

    [PRO] created Game Mode interface

---
+++ PewPew-BattleColosseum-/Assets/Code/Interfaces/IGameMode.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace PPBC {
+    public interface IGameMode {
+        
+        Sprite m_icon { get; }
+        string m_name { get; }
+        string m_text { get; }
+
+        bool m_isTeamMode { get; }
+
+        void StartGame();
+
+        void DoEndGame();
+
+        void AbortGame();
+
+        System.Action EndGame { get; set; }
+
+        e_mileStones[] GetMileStones();
+
+        //===== inGame =====
+
+        void PlayerDied(IHarmingObject killer, Player victim);
+
+        void ScorePoint(Player scorer);
+    }
+}
