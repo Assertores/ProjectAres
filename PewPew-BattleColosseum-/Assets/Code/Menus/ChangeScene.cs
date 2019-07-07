28803d6bef5db148b97c96dfcea5cd84505187c4
[Jam]Andreas <andreas.edmeier@web.de>
2019-07-07 13:20:09 +0200

    [PRO] created menu helper classes

---
+++ PewPew-BattleColosseum-/Assets/Code/Menus/ChangeScene.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace PPBC {
+    public class ChangeScene : MonoBehaviour {
+
+        #region Variables
+
+        [SerializeField] string m_nextScene = StringCollection.S_MAINMENU;
+
+        #endregion
+
+        public void DoChangeScene() {
+            TransitionHandler.ReadyToChange += ChangeSceneImmediately;
+
+            TransitionHandler.StartOutTransition();
+        }
+
+        public void ChangeSceneImmediately() {
+            SceneManager.LoadScene(m_nextScene);
+        }
+    }
+}
---
+++ PewPew-BattleColosseum-/Assets/Code/Menus/Exiter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PPBC {
+    public class Exiter : MonoBehaviour {
+
+        public void DisconnectPlayer() {
+            if(Player.s_references.Count <= 1) {
+                Application.Quit();
+                return;
+            }
+
+            TriggerButton.s_hoPlayer?.m_controler.DoDisconnect();
+        }
+    }
+}
