0ff7f658a0e432547f517ca0d8b5a93b81ef75c1
[Jam]Andreas <andreas.edmeier@web.de>
2019-07-07 13:42:25 +0200

    [PRO] created Map and Modi selection

---
+++ PewPew-BattleColosseum-/Assets/Code/RefHolders/SelectItemRefHolder.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+namespace PPBC {
+    public class SelectItemRefHolder : MonoBehaviour {
+
+        public SpriteRenderer r_image;
+        public TextMeshProUGUI r_name;
+        public TextMeshProUGUI r_text;
+    }
+}
