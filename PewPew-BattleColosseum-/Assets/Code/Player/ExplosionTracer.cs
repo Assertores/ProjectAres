fc507919d9f573a2bd15b418f709eb6c97ed87be
[Jam]Andreas <andreas.edmeier@web.de>
2019-07-07 13:19:26 +0200

    [PRO] added Rocket and Explosion

---
+++ PewPew-BattleColosseum-/Assets/Code/Player/ExplosionTracer.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PPBC {
+    public class ExplosionTracer : MonoBehaviour, ITracer {
+
+        #region Variables
+        [Header("References")]
+        [SerializeField] GameObject r_explosion;
+        [SerializeField] GameObject r_explosionStains;
+        [SerializeField] AudioSource fx_audio;
+        [SerializeField] AudioClip[] m_sounds;
+
+        [Header("Balancing")]
+        [SerializeField] float m_baseDamage = 1;
+        [SerializeField] float m_radius = 5;
+        [SerializeField] float m_baseKnockback = 300;
+        [SerializeField] AnimationCurve m_fallOff;
+        [SerializeField] float m_explosionTime = 0.5f;
+        [SerializeField] float m_halfPitchRange = 0.1f;
+        [SerializeField] float m_halfVolumeRange = 0.1f;
+
+        float m_time;
+
+        float m_startPitch;
+        float m_startVolume;
+
+        #endregion
+        #region MonoBehaviour
+
+        void Update() {
+            if (m_time + m_explosionTime < Time.timeSinceLevelLoad) {
+                Destroy(r_explosion);
+                r_explosionStains.SetActive(true);
+                Destroy(this.gameObject);
+
+            }
+        }
+
+        #endregion
+        #region ITracer
+
+        public IHarmingObject m_trace { get; private set; }
+
+        public Rigidbody2D Init(IHarmingObject trace) {
+            m_trace = trace;
+
+            r_explosion.SetActive(true);
+            m_time = Time.timeSinceLevelLoad;
+
+            if (m_sounds.Length > 0) {
+                fx_audio.pitch = Random.Range(fx_audio.pitch - m_halfPitchRange, fx_audio.pitch + m_halfPitchRange);
+                fx_audio.volume = Random.Range(fx_audio.volume - m_halfVolumeRange, fx_audio.volume + m_halfVolumeRange);
+                fx_audio.PlayOneShot(m_sounds[Random.Range(0, m_sounds.Length)]);
+            }
+
+            //CameraShake.DoCamerashake(0.1f, 0.7f);//TODO: reimpliment
+
+            foreach (var it in Physics2D.OverlapCircleAll(transform.position, m_radius)) {
+                IDamageableObject tmp = it.gameObject.GetComponent<IDamageableObject>();
+                if (tmp != null) {
+                    Vector2 dir = it.transform.position - transform.position;
+                    float fallOff = m_fallOff.Evaluate(dir.magnitude / m_radius);
+                    tmp.TakeDamage(m_trace, (fallOff * m_baseDamage), dir.normalized * fallOff * m_baseKnockback);
+                }
+            }
+            return null;
+        }
+
+        #endregion
+    }
+}
---
+++ PewPew-BattleColosseum-/Assets/Code/Player/RocketTracer.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PPBC {
+    public class RocketTracer : MonoBehaviour, ITracer {
+
+        #region Variables
+
+        [Header("References")]
+        [SerializeField] GameObject p_explosionRef;
+
+        [Header("Balancing")]
+        [Tooltip("distance form levelorigion to autodestry")]
+        [SerializeField] float m_killDistance;
+        [SerializeField] float m_damage;
+
+        Rigidbody2D m_rb;
+
+        #endregion
+        #region MonoBehaviour
+
+        void Update() {
+            if (transform.position.magnitude > m_killDistance) {
+                Destroy(this.gameObject);
+            }
+        }
+
+        #endregion
+        #region ITracer
+
+        public IHarmingObject m_trace { get; private set; }
+
+        public Rigidbody2D Init(IHarmingObject trace) {
+            m_trace = trace;
+
+            m_rb = GetComponent<Rigidbody2D>();
+            return m_rb;
+        }
+
+        #endregion
+        #region Physics
+
+        bool h_exploded = false;
+        private void OnCollisionEnter2D(Collision2D collision) {
+            if (h_exploded)
+                return;
+            h_exploded = true;
+
+            if (p_explosionRef) {
+                GameObject temp = Instantiate(p_explosionRef, transform.position, transform.rotation);
+                temp.GetComponentInChildren<ITracer>()?.Init(m_trace);
+            }
+
+            IDamageableObject tmp = collision.gameObject.GetComponent<IDamageableObject>();
+            if (tmp != null) {
+                tmp.TakeDamage(m_trace, m_damage, Vector2.zero/*m_rb.velocity * m_rb.mass*/);//rocket wont give recoil to the hit one
+            }
+
+            Destroy(gameObject);
+        }
+
+        #endregion
+    }
+}
