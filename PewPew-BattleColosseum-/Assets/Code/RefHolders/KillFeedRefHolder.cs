7398433c82cfb29108511c5bb218f84890185943
[GA] Andreas <andreas.edmeier@web.de>
2019-07-11 14:44:33 +0200

    [PRO] added KillFeed

---
+++ PewPew-BattleColosseum-/Assets/Code/RefHolders/KillFeedRefHolder.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PPBC {
+    public class KillFeedRefHolder : MonoBehaviour {
+
+        public SpriteRenderer m_killer;
+        public SpriteRenderer m_weapon;
+        public SpriteRenderer m_victim;
+    }
+}
