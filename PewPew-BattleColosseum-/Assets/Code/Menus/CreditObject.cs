9cfe462236e9810ddf505214474dfd3fd987c56a
[GA] Andreas <andreas.edmeier@web.de>
2019-07-11 14:49:56 +0200

    [PRO] created Credits shootable

---
+++ PewPew-BattleColosseum-/Assets/Code/Menus/CreditObject.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PPBC {
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class CreditObject : MonoBehaviour, IDamageableObject {
+
+        Rigidbody2D m_rb;
+
+        #region MonoBehaviour
+
+        private void Awake() {
+            m_rb = GetComponent<Rigidbody2D>();
+        }
+
+        #endregion
+        #region IDamageableObject
+
+        public bool m_alive => true;
+
+        public void Die(IHarmingObject source, bool doTeamDamage = true) {
+            return;
+        }
+
+        public void TakeDamage(IHarmingObject source, float damage, Vector2 recoilDir, bool doTeamDamage = true) {
+            m_rb.AddForce(recoilDir);
+        }
+
+        #endregion
+    }
+}
