3fdda1f76262ea93b374efe05114f15d9446e42b
[Jam]Andreas <andreas.edmeier@web.de>
2019-07-13 13:42:14 +0200

    [PRO] filled TeamsSelect and Credits

---
+++ PewPew-BattleColosseum-/Assets/Code/Tools/SpawnPlayerAtStart.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PPBC {
+    public class SpawnPlayerAtStart : MonoBehaviour {
+
+        private void Start() {
+            foreach (var it in Player.s_references) {
+                it.Respawn(SpawnPoint.s_references[0].transform.position);
+            }
+            Destroy(this);
+        }
+    }
+}
