19db99b4c0c7af4de8edc3a56c1805652825b478
[GA] Andreas <andreas.edmeier@web.de>
2019-07-15 16:14:21 +0200

    [PRO] try remove postProcessing from icons

--- PewPew-BattleColosseum-/Assets/Code/Map/ScreenshotCam.cs
+++ PewPew-BattleColosseum-/Assets/Code/Map/ScreenshotCam.cs
@@ -25,14 +25,16 @@ namespace PPBC {
                 m_cam.orthographicSize = (FitCameraToAABB.m_aABB.size.x / 2) / m_cam.aspect;
             }
 
+            var holder = RenderTexture.active;
+
+            RenderTexture.active = m_texture;
+
             m_cam.gameObject.SetActive(true);
             m_cam.Render();
             m_cam.gameObject.SetActive(false);
 
             Texture2D value = new Texture2D(m_texture.width, m_texture.height);
-            var holder = RenderTexture.active;
-
-            RenderTexture.active = m_texture;
+            
             value.ReadPixels(new Rect(0, 0, value.width, value.height), 0, 0, false);
             RenderTexture.active = holder;
 
