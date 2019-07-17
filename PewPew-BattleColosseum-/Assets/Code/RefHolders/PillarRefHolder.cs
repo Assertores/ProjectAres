83e54000f4c89c8c755e02243493237f83e338b3
[GA] Andreas <andreas.edmeier@web.de>
2019-07-17 12:12:54 +0200

    [PRO] implimented piller funktionality

--- PewPew-BattleColosseum-/Assets/Code/RefHolders/PillarRefHolder.cs
+++ PewPew-BattleColosseum-/Assets/Code/RefHolders/PillarRefHolder.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 namespace PPBC {
     public class PillarRefHolder : MonoBehaviour {
 
+        public TextMeshProUGUI r_points;
+
         //calculation variables
-        public Vector3 m_holderPos;
-        public Vector3 m_middlePos;
-        public Vector3 m_targetPos;
-        public int m_previousMatchPoints;
+        [HideInInspector] public Vector3 m_holderPos;
+        [HideInInspector] public Vector3 m_middlePos;
+        [HideInInspector] public Vector3 m_targetPos;
+        [HideInInspector] public int m_previousMatchPoints;
     }
 }
