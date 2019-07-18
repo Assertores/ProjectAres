58a8b3ab99b5612e7c30bca0b8bd6bdd19907002
[GA] Andreas <andreas.edmeier@web.de>
2019-07-18 11:43:43 +0200

    [PRO] implimented sfx

--- PewPew-BattleColosseum-/Assets/Code/Player/BulletTracer.cs
+++ PewPew-BattleColosseum-/Assets/Code/Player/BulletTracer.cs
@@ -7,6 +7,16 @@ namespace PPBC {
     [RequireComponent(typeof(Collider2D))]
     public class BulletTracer : MonoBehaviour, ITracer {
 
+        enum e_impactEffects { Player, Level, CreditsObject};
+
+        [System.Serializable]
+        struct d_imactEffects {
+            public e_impactEffects m_type;
+            public AudioClip[] m_clips;
+
+            [HideInInspector] public string m_typeString;
+        }
+
         #region Variables
 
         [Header("References")]
@@ -14,6 +24,8 @@ namespace PPBC {
         [SerializeField] ContactFilter2D m_filter;
         [SerializeField] GameObject r_wallHitParent;
         [SerializeField] ParticleSystem Vfx_wallHit;
+        [SerializeField] AudioSource m_audio;
+        [SerializeField] d_imactEffects[] m_sounds;
 
         [Header("Balancing")]
         [SerializeField] float m_killDistance = 1000;
@@ -40,6 +52,22 @@ namespace PPBC {
                     break;
                 }
             }
+
+            for (int i = 0; i < m_sounds.Length; i++) {
+                switch (m_sounds[i].m_type) {
+                case e_impactEffects.Player:
+                    m_sounds[i].m_typeString = StringCollection.T_PLAYER;
+                    break;
+                case e_impactEffects.Level:
+                    m_sounds[i].m_typeString = StringCollection.T_LEVEL;
+                    break;
+                case e_impactEffects.CreditsObject:
+                    m_sounds[i].m_typeString = StringCollection.T_CREOBJ;
+                    break;
+                default:
+                    break;
+                }
+            }
         }
 
         private void Update() {
@@ -62,9 +90,13 @@ namespace PPBC {
 
         #endregion
 
-        IEnumerator IEEffects() {
+        IEnumerator IEEffects(string impactTag) {
             Vfx_wallHit.Play();
-            //Play Audio
+            int i = 0;
+            for (; i < m_sounds.Length && m_sounds[i].m_typeString != impactTag; i++) ;
+            if(i < m_sounds.Length) {
+                m_audio.PlayOneShot(m_sounds[i].m_clips[Random.Range(0, m_sounds[i].m_clips.Length)]);
+            }
 
             yield return new WaitForSeconds(Vfx_wallHit.main.duration);
             Destroy(this.gameObject);//TODO: objectPooling
@@ -92,7 +124,7 @@ namespace PPBC {
             m_col.enabled = false;
             r_bullet.SetActive(false);
 
-            StartCoroutine(IEEffects());
+            StartCoroutine(IEEffects(collision.tag));
         }
 
         private void OnTriggerExit2D(Collider2D collision) {
