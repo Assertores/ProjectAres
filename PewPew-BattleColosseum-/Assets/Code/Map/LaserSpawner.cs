d02ba4437f447e1aac3bf7b3f8ab244de56e71d6
[GA] Andreas <andreas.edmeier@web.de>
2019-07-21 14:58:25 +0200

    [PRO] Map stuff
    - Stage no mask
    - props on LaserSpawner stay activ
    - LaserBehaviour deterministic (map index = seed)

--- PewPew-BattleColosseum-/Assets/Code/Map/LaserSpawner.cs
+++ PewPew-BattleColosseum-/Assets/Code/Map/LaserSpawner.cs
@@ -3,14 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace PPBC {
+    [RequireComponent(typeof(Collider2D))]
     public class LaserSpawner : MonoBehaviour {
 
         public static List<LaserSpawner> s_references = new List<LaserSpawner>();
         public int m_index;
         public GameObject fx_on;
+        [SerializeField] ContactFilter2D m_filter;
+
+        List<GameObject> m_ActiveList = new List<GameObject>();
 
         private void Awake() {
             s_references.Add(this);
+        }
+
+        private void Start() {
+            Collider2D[] tmp = new Collider2D[10];
+            int count = GetComponent<Collider2D>().OverlapCollider(m_filter, tmp);
+            for (int i = 0; i < count; i++) {
+                m_ActiveList.Add(tmp[i].gameObject);
+            }
         }
 
         private void OnDestroy() {
@@ -20,6 +32,12 @@ namespace PPBC {
         public void Init(int index) {
             m_index = index;
             s_references.Sort((LaserSpawner lhs, LaserSpawner rhs) => lhs.m_index.CompareTo(rhs.m_index));
+        }
+
+        public void Reactivate() {
+            foreach(var it in m_ActiveList) {
+                it.SetActive(true);
+            }
         }
     }
 }
