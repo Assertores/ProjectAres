b1e57694f41a6a2fa2c1db8b1902a65d4ab88973
[GA] Andreas <andreas.edmeier@web.de>
2019-07-24 14:57:45 +0200

    [PRO] adapted modis to new Interface

--- PewPew-BattleColosseum-/Assets/Code/GameModis/FFA.cs
+++ PewPew-BattleColosseum-/Assets/Code/GameModis/FFA.cs
@@ -29,8 +29,9 @@ namespace PPBC {
 
         public System.Action<bool> EndGame { get; set; }
 
-        public void StartTransition() {
+        public float StartTransition() {
             //Player transition
+            return 0;
         }
 
         public void SetUpGame() {
