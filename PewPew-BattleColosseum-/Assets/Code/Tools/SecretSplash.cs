e99d4c99b164b271b2d86793b29ab1fc8aec26bc
[GA] Andreas <andreas.edmeier@web.de>
2019-07-25 12:27:13 +0200

    [PRO] added NDA Splash script

---
+++ PewPew-BattleColosseum-/Assets/Code/Tools/SecretSplash.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+namespace PPBC {
+    public class SecretSplash : MonoBehaviour {
+
+        #region Variables
+
+        [SerializeField] VideoPlayer r_player;
+        [SerializeField] VideoClip p_speticalClip;
+        [SerializeField] string m_commandLineArg;
+
+        #endregion
+        #region MonoBehaviour
+
+        private void Awake() {
+            if(m_commandLineArg != "" && r_player != null && p_speticalClip != null) {
+                foreach (string it in System.Environment.GetCommandLineArgs()) {
+                    if (it == m_commandLineArg) {
+                        r_player.clip = p_speticalClip;
+                        break;
+                    }
+                }
+            }
+
+            Destroy(this);
+        }
+
+        #endregion
+    }
+}
