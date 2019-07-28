33335af657bae5ef47863fd658220461df204bdf
[GA] Andreas <andreas.edmeier@web.de>
2019-07-28 14:17:36 +0200

    [GEN] some Main menu updates

---
+++ PewPew-BattleColosseum-/Assets/Code/Tools/NDA.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace PPBC {
+    public class NDA : MonoBehaviour {
+
+        [SerializeField] float m_delay = 2;
+
+        private void Start() {
+            StartCoroutine(Wait());
+        }
+
+        IEnumerator Wait() {
+            yield return new WaitForSeconds(m_delay);
+            TransitionHandler.ReadyToChange += ReturnImideatly;
+
+            TransitionHandler.StartOutTransition();
+        }
+
+        void ReturnImideatly() {
+            TransitionHandler.ReadyToChange -= ReturnImideatly;
+
+            SceneManager.LoadScene(StringCollection.S_MAINMENU);
+        }
+    }
+}
