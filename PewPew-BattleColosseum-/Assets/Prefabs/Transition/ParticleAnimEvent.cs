33e33577d194527bb11e384d00eade9af306e155
[GA] Max <maximilian_raabe@icloud.com>
2019-08-01 23:01:33 +0200

    [GEN] Transition fancy (without smoke though)

---
+++ PewPew-BattleColosseum-/Assets/Prefabs/Transition/ParticleAnimEvent.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PPBC { 
+
+    public class ParticleAnimEvent : MonoBehaviour
+    {
+        [SerializeField] ParticleSystem FX_smokeCurtain;
+
+        public void DoSmokeEffect() {
+            FX_smokeCurtain.Play();
+        }
+    }
+}
