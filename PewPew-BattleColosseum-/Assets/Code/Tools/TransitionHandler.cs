114eb63974e879666632da5f11ced98b9855ad2c
[GA] Andreas <andreas.edmeier@web.de>
2019-08-01 13:09:07 +0200

    [PRO] Game Transition started work on

--- PewPew-BattleColosseum-/Assets/Code/Tools/TransitionHandler.cs
+++ PewPew-BattleColosseum-/Assets/Code/Tools/TransitionHandler.cs
@@ -51,6 +51,13 @@ namespace PPBC {
                 ReadyToChange?.Invoke();
         }
 
+        public static void StartGameTransition() {
+            if (s_singelton.r_anim)
+                s_singelton.StartCoroutine(s_singelton.IEGameTransition());
+            else
+                ReadyToChange?.Invoke();
+        }
+
         IEnumerator IEInTransition() {
             yield return new WaitForSeconds(r_anim.GetCurrentAnimatorClipInfo(0)[0].clip.length);
             ReadyToStart?.Invoke();
@@ -60,6 +67,48 @@ namespace PPBC {
             r_anim.SetBool("FadeOut", true);
             yield return new WaitForSeconds(r_anim.GetCurrentAnimatorClipInfo(0)[0].clip.length);
             ReadyToChange?.Invoke();
+        }
+
+        IEnumerator IEGameTransition() {
+            ReadyToChange?.Invoke();
+            Animation anim = null;
+
+            //richtige animation auswählen
+            //richtige informationen in animation geben
+            if (DataHolder.s_modis[DataHolder.s_currentModi].m_isTeamMode) {
+                List<Player> teamA = Player.s_references.FindAll(x => x.m_team == 0);
+                List<Player> teamB = Player.s_references.FindAll(x => x.m_team == 1);
+                if(teamA.Count == 2 && teamB.Count == 2) {
+                    //team 2vs2
+                }else if(teamA.Count == 3 && teamB.Count == 1) {
+                    //team 3vs1
+                }else if(teamA.Count == 1 && teamB.Count == 3) {
+                    //team 1vs3
+                }
+            } else {
+                switch (Player.s_references.Count) {
+                case 2:
+                    //ffa 2
+                    break;
+                case 3:
+                    //ffa 3
+                    break;
+                case 4:
+                    //ffa 4
+                    break;
+                default:
+                    break;
+                }
+            }
+
+            if (anim) {
+                anim.gameObject.SetActive(true);
+                anim.Play();
+                yield return new WaitForSeconds(anim.clip.length);
+                anim.gameObject.SetActive(false);
+            }
+
+            ReadyToStart?.Invoke();
         }
     }
 }
