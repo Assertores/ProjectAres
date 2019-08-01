07ad1876fbc5cae1bf8d9420115ba657b16f1839
[GA] Andreas <andreas.edmeier@web.de>
2019-08-01 13:08:47 +0200

    [GEN] transitions imported

---
+++ PewPew-BattleColosseum-/Assets/Code/RefHolders/GameTransitionRefHolder.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PPBC {
+    public class GameTransitionRefHolder : MonoBehaviour {
+
+        public Animator r_anim;
+
+        public SpriteRenderer r_p1;
+        public SpriteRenderer r_p2;
+        public SpriteRenderer r_p3;
+        public SpriteRenderer r_p4;
+
+        [Tooltip("or 1 team")]
+        public SpriteRenderer r_p1Background;
+        [Tooltip("or 2 team")]
+        public SpriteRenderer r_p2Background;
+        public SpriteRenderer r_p3Background;
+        public SpriteRenderer r_p4Background;
+    }
+}
