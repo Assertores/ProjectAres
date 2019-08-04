2c878d1d3eaed2bf84fd7d9b054f80b0e1a5bf60
[GA] Max <maximilian_raabe@icloud.com>
2019-08-04 14:45:59 +0200

    [GEN] Transitions Fixed

--- PewPew-BattleColosseum-/Assets/Prefabs/Transition/TransitionAnimation.cs
+++ PewPew-BattleColosseum-/Assets/Prefabs/Transition/TransitionAnimation.cs
@@ -7,9 +7,11 @@ public class TransitionAnimation : MonoBehaviour
     [SerializeField] ParticleSystem[] FX_vsEffect;
 
    public void DoVSEffect() {
+        print("Ping");
         for (int i = 0; i < FX_vsEffect.Length; i++) {
             FX_vsEffect[i].Play();
+            Debug.Log(FX_vsEffect);
         }
-        
+        print("Pong");
     }
 }
