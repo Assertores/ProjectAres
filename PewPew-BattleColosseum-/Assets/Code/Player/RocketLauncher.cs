18b9cffa0d30b5da9b6d9196062343c70cd0ee05
[GA] Andreas <andreas.edmeier@web.de>
2019-08-06 20:55:34 +0200

    [PRO] rocked fix

--- PewPew-BattleColosseum-/Assets/Code/Player/RocketLauncher.cs
+++ PewPew-BattleColosseum-/Assets/Code/Player/RocketLauncher.cs
@@ -122,6 +122,8 @@ namespace PPBC {
         }
 
         void Overcharged() {
+            m_lastShot = Time.time;
+
             Instantiate(m_owner.m_modelRef.m_rocket.p_explosion, m_owner.m_modelRef.m_rocket.r_barrel.position, m_owner.m_modelRef.m_rocket.r_barrel.rotation).GetComponent<ITracer>()?.Init(this);
             m_owner.m_modelRef.m_rocket.r_RocketAnim.AnimationState.SetAnimation(0, StringCollection.AR_IDLE, true);
             print("B");
