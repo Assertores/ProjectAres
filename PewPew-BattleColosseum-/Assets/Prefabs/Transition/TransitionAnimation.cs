1bdeec29eeee4a926ca939d7931e5a813d7f6d0a
[GA] Andreas <andreas.edmeier@web.de>
2019-08-07 12:15:46 +0200

    [PRO] added VS audio Effect

--- PewPew-BattleColosseum-/Assets/Prefabs/Transition/TransitionAnimation.cs
+++ PewPew-BattleColosseum-/Assets/Prefabs/Transition/TransitionAnimation.cs
@@ -2,16 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TransitionAnimation : MonoBehaviour
-{
-    [SerializeField] ParticleSystem[] FX_vsEffect;
+namespace PPBC {
+    public class TransitionAnimation : MonoBehaviour {
+        [SerializeField] ParticleSystem[] FX_vsEffect;
+        [SerializeField] AudioClip fx_vsAudio;
 
-   public void DoVSEffect() {
-        print("Ping");
-        for (int i = 0; i < FX_vsEffect.Length; i++) {
-            FX_vsEffect[i].Play();
-            Debug.Log(FX_vsEffect);
+        public void DoVSEffect() {
+            print("Ping");
+            for (int i = 0; i < FX_vsEffect.Length; i++) {
+                FX_vsEffect[i].Play();
+                Debug.Log(FX_vsEffect);
+            }
+            GetComponent<AudioSource>().PlayOneShot(fx_vsAudio);
+            print("Pong");
         }
-        print("Pong");
     }
 }
